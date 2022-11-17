ccf46a466d128c23d86642f2345c4a76bfaffd8b
tomdv18 <72037059+tomdv18@users.noreply.github.com>
2022-11-17 16:46:43 -0300

    punto 1

--- fourier1.m
+++ fourier1.m
@@ -1,7 +1,7 @@
 function [coeficientes, ecm] = fourier1(alturas)
   cantidad_horas = length(alturas)
   x = [1:1:cantidad_horas];
-  y = alturas;
+  y = alturas';
 
   fi1 = ones(1,cantidad_horas);
   fi2 = cos(((2 * pi)/cantidad_horas) * 7 * x);
@@ -9,7 +9,7 @@ function [coeficientes, ecm] = fourier1(alturas)
 
 
   M=[fi1*fi1' fi1*fi2' fi1*fi3'; fi2*fi1' fi2*fi2' fi2*fi3'; fi3*fi1' fi3*fi2' fi3*fi3'];
-  b = [y*fi1; y*fi2; y*fi3b];
+  b = [y*fi1'; y*fi2'; y*fi3'];
   coeficientes = inv(M)*b;
 
   ycalc = coeficientes(1)*fi1 +coeficientes(2)*fi2 + coeficientes(3)*fi3;
