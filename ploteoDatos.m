87723fa2066e6b177a2706d36a952a42b70dc249
tomdv18 <72037059+tomdv18@users.noreply.github.com>
2022-11-17 15:11:43 -0300

    Act ploteo datos

--- ploteoDatos.m
+++ ploteoDatos.m
@@ -1,6 +1,7 @@
 function alturas = ploteoDatos(cantidad_dias)
  datos = csvread("ArchivosCSV/Point_Reyes_CA_2020.csv");
- cantidad_mediciones = length(datos)
- alturas = datos(2:cantidad_mediciones,5); #Me quedo con la 5ta columna
- plot(alturas(1:cantidad_dias * 24)) #El 24 es por las horas de cada dia
+ total_mediciones = length(datos)
+ alturas = datos(2:total_mediciones,5); #Me quedo con la 5ta columna
+ alturas = alturas(1:cantidad_dias * 24); #El 24 es por las horas de cada dia
+ plot(alturas)
   endfunction
